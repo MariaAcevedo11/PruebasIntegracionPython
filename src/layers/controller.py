ced9c32d4601f46ee00b0a508429da81ad2cd857
MariaAcevedo11 <macevedos2@eafit.edu.co>
2026-03-16 10:25:24 -0500

    noDio

--- src/layers/controller.py
+++ src/layers/controller.py
@@ -10,3 +10,11 @@ class UserController:
     def get_user_full_name(self, user_id: int) -> str:
         full = self.service.get_full_name(user_id)
         return "404 NOT_FOUND" if full is None else full
+
+# Si cambias el formato de salida del controlador. ¿Qué otras capas tendrías que adaptar?
+'''
+Es depende de qué se cambie en la salida, 
+se debería de cambiar entonces el servicio porque el controlador depende de el 
+y el servicio depende del respositorio
+
+'''
