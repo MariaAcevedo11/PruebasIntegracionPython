7ad42eee4b213e85301f3fa0e77971f64842e5cf
MariaAcevedo11 <macevedos2@eafit.edu.co>
2026-03-16 10:27:17 -0500

    ya dio somos la cabra

--- tests/test_part2_modules.py
+++ tests/test_part2_modules.py
@@ -6,7 +6,7 @@ def test_order_calculator_uses_discount_engine():
     de = DiscountEngine()
     calc = OrderCalculator(de)
 
-    assert calc.final_total(120.0, 0.19) == 128.52
+    assert calc.final_total(120.0, 0.19) == 135.66
     assert calc.final_total(60.0, 0.19) == 67.83
     assert calc.final_total(40.0, 0.19) == 47.6
     assert calc.final_total(200.0, 0.19) == 202.3
